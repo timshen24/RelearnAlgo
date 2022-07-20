c48019fb9d90a4d56f34f9de222bebd9ec46220b
tim.shen <tim.shen@farfetch.com>
2022-07-20 16:18:21 +0800

    add topological_sort.py

--- topological_sort.py
+++ topological_sort.py
@@ -4,26 +4,28 @@ from collections import deque
 def count_graph(graph):
 	counts = {node: 0 for node in graph}
 	for parent in graph:
-		for node in graph[parent]:
-			counts[node] += 1
+		for child in graph[parent]:
+			counts[child] += 1
 	return counts
 
 
 def topo_sort(graph):
+	queue = deque()
 	res = []
-	q = deque()
 	counts = count_graph(graph)
-	for node in counts:
-		if counts[node] == 0:
-			q.append(node)
-	while len(q) > 0:
-		node = q.popleft()
+	for parent, count in counts.items():
+		if count == 0:
+			queue.append(parent)
+	while queue:
+		node = queue.popleft()
 		res.append(node)
 		for child in graph[node]:
 			counts[child] -= 1
 			if counts[child] == 0:
-				q.append(child)
-	return res if len(graph) == len(res) else None
+				queue.append(child)
+	return res if len(res) == len(graph) else None
 
 
+graph = {4: [2], 5: [2], 6: [3], 2: [1], 3: [1], 1: []}
+print(count_graph(graph))
 print(topo_sort({4: [2], 5: [2], 6: [3], 2: [1], 3: [1], 1: []}))
