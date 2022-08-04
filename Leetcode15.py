4e61667989e372bbe8973706b560241ad350ae19
tim.shen <tim.shen@farfetch.com>
2022-08-05 06:43:13 +0800

    Leetcode15.py

--- Leetcode15.py
+++ Leetcode15.py
@@ -3,9 +3,8 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         res = []
-        for i, num in enumerate(nums):
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             l, r = i + 1, len(nums) - 1
