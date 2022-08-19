e06d6c561b6554112e690717f403a56efc615c88
tim.shen <tim.shen@farfetch.com>
2022-08-19 18:00:18 +0800

    knapsack_weight_only_3.py

---
+++ dp/knapsack_weight_only_3.py
@@ -0,0 +1,28 @@
+# Input
+# weights: A list of items and their weights.
+weights = [1, 3, 3, 5]
+# weights = [7, 49, 73, 58, 30, 72, 44, 78, 23, 9, 40, 65, 92, 42, 87, 3, 27, 29, 40, 12]
+
+# Output
+# A list of possible sums using the weights.
+# Output: [0, 1, 3, 4, 5, 6, 7, 8, 9, 11, 12]
+
+n = len(weights)
+max_weights = sum(weights)
+
+dp = [[False] * (max_weights + 1) for _ in range(n + 1)]
+
+
+def weight_only():
+    dp[0][0] = True
+    for i in range(1, len(dp)):
+        for j in range(len(dp[0])):
+            dp[i][j] = dp[i][j] | dp[i-1][j]
+            if j >= weights[i-1]:
+                dp[i][j] = dp[i][j] | dp[i-1][j-weights[i-1]]
+
+
+weight_only()
+print(dp)
+print(dp[-1])
+print([i for i, num in enumerate(dp[-1]) if num])
