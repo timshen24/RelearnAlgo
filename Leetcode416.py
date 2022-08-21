57869ee1dabaf89b1bc9b747240ba76a4707d59b
tim.shen <tim.shen@farfetch.com>
2022-08-22 06:30:42 +0800

    Leetcode416.py

---
+++ Leetcode416.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2:
+            return False
+        target = total // 2
+        dp = [True] + [False] * target
+        for i, num in enumerate(nums):
+            for j in range(target, num - 1, -1):
+                dp[j] |= dp[j - num]
+        return dp[-1]
+
+
+solution = Solution()
+print(solution.canPartition([1, 5, 11, 5]))
