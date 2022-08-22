f90775bb2e145626cacea01ae2c031609e0d9f38
tim.shen <tim.shen@farfetch.com>
2022-08-23 07:36:34 +0800

    Leetcode977.py

---
+++ Leetcode977.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l, r = 0, len(nums) - 1
+        ptr = r
+        res = [0] * len(nums)
+        while l <= r:
+            if abs(nums[l]) < abs(nums[r]):
+                res[ptr] = nums[r] ** 2
+            else:
+                res[ptr] = nums[l] ** 2
+            ptr -= 1
+        return res
+
+
+solution = Solution()
+print(solution.sortedSquares([-4, -1, 0, 3, 10]))
