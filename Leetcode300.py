0b7fc9aa1fc2d5387d90100e3ff74028c2576c3b
tim.shen <tim.shen@farfetch.com>
2022-08-24 14:47:40 +0800

    Leetcode300.py

--- Leetcode300.py
+++ Leetcode300.py
@@ -14,3 +14,23 @@ class Solution:
 solution = Solution()
 print(solution.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
 print(solution.lengthOfLIS([0, 1, 0, 3, 2, 3]))
+
+
+class Solution1:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        greedy = []
+        for num in nums:
+            if not greedy or greedy[-1] < num:
+                greedy.append(num)
+            else:
+                l, r = 0, len(greedy)
+                loc = r-1
+                while l < r:
+                    mid = (l+r)//2
+                    if greedy[mid] >= num:
+                        loc = mid
+                        r -= 1
+                    else:
+                        l += 1
+                greedy[loc] = num
+        return len(greedy)
