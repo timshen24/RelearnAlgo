54575c0567881ff28c820d87eaed012b62db184b
tim.shen <tim.shen@farfetch.com>
2022-08-24 12:00:43 +0800

    Leetcode977.py Leetcode300.py

--- Leetcode300.py
+++ Leetcode300.py
@@ -4,13 +4,11 @@ from typing import List
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1] * len(nums)
-        res = 0
-        for i in range(len(nums)):
+        for i in range(1, len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
-            res = max(res, dp[i])
-        return res
+        return max(dp)
 
 
 solution = Solution()
