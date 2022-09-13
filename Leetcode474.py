e8909821253367aa9726201d2f7c0a01aaa92701
tim.shen <tim.shen@farfetch.com>
2022-09-13 10:45:26 +0800

    leetcode

--- Leetcode474.py
+++ Leetcode474.py
@@ -3,16 +3,16 @@ from typing import List
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         length = len(strs)
-        dp = [[[0] * (n + 1) for _ in range(m + 1)] for _ in range(length+1)]
+        dp = [[[0] * (n+1) for _ in range(m+1)] for _ in range(length + 1)]
         for i in range(1, length + 1):
-            mCount, nCount = strs[i-1].count('0'), strs[i-1].count('1')
+            zeroes = strs[i-1].count('0')
+            ones = len(strs[i-1]) - zeroes
             for j in range(m + 1):
                 for k in range(n + 1):
-                    if j >= mCount and k >= nCount:
-                        dp[i][j][k] = max(dp[i - 1][j][k], dp[i - 1][j - mCount][k - nCount] + 1)
+                    if j >= zeroes and k >= ones:
+                        dp[i][j][k] = max(dp[i-1][j][k], dp[i-1][j-zeroes][k-ones] + 1)
                     else:
                         dp[i][j][k] = dp[i-1][j][k]
-        print(dp)
         return dp[length][m][n]
 
 
