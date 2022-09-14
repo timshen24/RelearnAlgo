b444aa63b57789fc93dfdd3054b45cb8dcc93aa5
tim.shen <tim.shen@farfetch.com>
2022-09-14 11:45:13 +0800

    leetcode

---
+++ Leetcode134.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        currentStart = None
+        currentGas = currentLocation = 0
+        while currentLocation < n * 2:
+            if not currentStart:
+                currentStart = currentLocation
+            currentGas += gas[currentLocation % n] - cost[currentLocation % n]
+            if currentGas < 0:π
+                currentStart = None
+                currentGas = 0
+            currentLocation += 1
+            if currentStart and currentLocation - currentStart == n:
+                return currentStart % n
+        return -1
+
+
+solution = Solution()
+print(solution.canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
+print(solution.canCompleteCircuit([3, 1, 1], [1, 2, 2]))
