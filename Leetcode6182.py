d7498a3dd2645374710970f14f7a2386d614e865
tim.shen <tim.shen@farfetch.com>
2022-09-18 13:25:42 +0800

    leetcode

---
+++ Leetcode6182.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(left, right, level):
+            if not left:
+                return
+            if level % 2 == 0:
+                left.val, right.val = right.val, left.val
+            dfs(left.left, right.right, level + 1)
+            dfs(left.right, right.left, level + 1)
+        dfs(root.left, root.right, 0)
+        return root
