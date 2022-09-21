06bcdd8115ef8f7a583145a6c1cae6d80d987940
tim.shen <tim.shen@farfetch.com>
2022-09-21 17:42:37 +0800

    leetcode

--- Leetcode662.py
+++ Leetcode662.py
@@ -13,19 +13,19 @@ class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         res = 0
         root.val = 1
-        q = deque([root])
-        while q:
-            minId=maxId=-1
-            for _ in range(len(q)):
-                node = q.popleft()
+        queue = deque([root])
+        while queue:
+            minId = maxId = -1
+            for _ in range(len(queue)):
+                node = queue.popleft()
                 maxId = node.val
                 if minId == -1:
-                    minId = node.val
+                    minId = maxId
                 if node.left:
                     node.left.val = maxId << 1
-                    q.append(node.left)
+                    queue.append(node.left)
                 if node.right:
                     node.right.val = maxId << 1 | 1
-                    q.append(node.right)
-            res = max(res, maxId-minId+1)
+                    queue.append(node.right)
+            res = max(res, maxId - minId + 1)
         return res
