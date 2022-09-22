f454c0b1193888c174decccfe0ef8cfde1554e5e
tim.shen <tim.shen@farfetch.com>
2022-09-22 22:45:31 +0800

    leetcode

---
+++ unionfind/UnionFind5.py
@@ -0,0 +1,23 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+
+    def find(self, x):
+        y = self.parent.get(x, x)
+        if y != x:
+            self.parent[x] = y = self.find(y)
+        return y
+
+    def union(self, x, y):
+        if self.find(x) not in self.rank:
+            self.rank[self.find(x)] = 0
+
+        if self.find(y) not in self.rank:
+            self.rank[self.find(y)] = 0
+        if self.rank[self.find(x)] < self.rank[self.find(y)]:
+            self.parent[self.find(x)] = self.find(y)
+        else:
+            self.parent[self.find(y)] = self.find(x)
+            if self.rank[self.find(x)] == self.rank[self.find(y)]:
+                self.rank[self.find(x)] = self.rank[self.find(x)] + 1
