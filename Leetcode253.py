d8917ef26a1f9d75873d547968b2af20288aaea9
tim.shen <tim.shen@farfetch.com>
2022-09-24 11:43:42 +0800

    leetcode

--- Leetcode253.py
+++ Leetcode253.py
@@ -18,5 +18,17 @@ class Solution:
         return res
 
 
-solution = Solution()
+class Solution1:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        heap = [intervals[0][1]]
+        for interval in intervals[1:]:
+            if interval[0] >= heap[0]:
+                heappop(heap)
+            heappush(heap, interval[1])
+        return len(heap)
+
+
+solution = Solution1()
 print(solution.minMeetingRooms([[0, 30], [5, 10], [15, 20]]))
+print(solution.minMeetingRooms([[6, 15], [13, 20], [6, 17]]))
